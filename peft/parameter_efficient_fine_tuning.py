1654f696678b3392cbbe41f6f196c4b843ede1b8
Justin Lee <leezzjus@amazon.com>
2023-10-26 21:01:34 -0700

    Loads dataset using datasets module

--- peft/parameter_efficient_fine_tuning.py
+++ peft/parameter_efficient_fine_tuning.py
@@ -6,34 +6,65 @@ import os
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-# TK follow this tutorial: https://huggingface.co/docs/peft/task_guides/clm-prompt-tuning
+
 
 device = "cpu" # can change to "cuda"
+
+
+
+
+
+
+
+print("\n\n")
+print("=== Defining PEFT config ===") # Follow this tutorial: https://huggingface.co/docs/peft/task_guides/clm-prompt-tuning
+
 model_name_or_path = "meta-llama/Llama-2-7b-chat-hf"
-tokenizer_name_or_path = "meta-llama/Llama-2-7b-chat-hf"
+
 peft_config = PromptTuningConfig(
-    task_type=TaskType.CAUSAL_LM, # could also be QUESTION ANSWER
+    task_type=TaskType.CAUSAL_LM, # could also be QUESTION_ANS
     prompt_tuning_init=PromptTuningInit.TEXT,
-    num_virtual_tokens=8, # Length of tokens that is added to the prompt
+    num_virtual_tokens=8, # length of tokens that is added to the prompt
     prompt_tuning_init_text="Determine whether the statement is true or false, and then provide reasoning:",
     tokenizer_name_or_path=model_name_or_path,
 )
 
-text_column = "Tweet text"
-label_column = "text_label"
 max_length = 64
 lr = 3e-2
 num_epochs = 50
 batch_size = 8
 
-#TK figure out out to load the model from https://huggingface.co/docs/transformers/model_doc/llama2
 
 
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf", token="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")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", token="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")
-# print(model.print_trainable_parameters())
- 
-#asdf
+
+
+
+
+
+print("=== Loading model and tokenizer ===")
+# tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf", token="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")
+# model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", token="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")
+
+
+
+
+
+
+print("=== Loading dataset ===")
+
+generic_column = "generic_new"
+exemplar_column = "exemplar"
+dataset = load_dataset("../exemplars")
+
+
+
+
+print("=== Preprocess dataset ===")
+
+
+
+
+
 
 
 
