f5b59f58db7122a0d74a17e5c0380318c03de13e
Justin Lee <leezzjus@amazon.com>
2023-11-03 18:53:26 -0700

    Minor

--- peft/parameter_efficient_fine_tuning.py
+++ peft/parameter_efficient_fine_tuning.py
@@ -41,38 +41,32 @@ def main():
     model = AutoModelForCausalLM.from_pretrained(model_name_or_path, token="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")
     model = get_peft_model(model, peft_config)
     model.print_trainable_parameters()
-
-
-
-
     accelerator = Accelerator()
 
 
 
-    # print("\n=== Loading dataset ===")
+    print("\n=== Loading dataset ===")
 
-    # text_column = "text_input"
-    # label_column = "text_label"
-    # dataset = load_dataset("../exemplars-raw")
+    text_column = "text_input"
+    label_column = "text_label"
+    dataset = load_dataset("../exemplars-raw")
 
-    # dataset = dataset.map(
-    #     lambda x: {
-    #         text_column: ["All " + i.lower() for i in x["generic_new"]] + ["Not all " + i.lower() for i in x["generic_new"]],
-    #         label_column: ["False, " + i.lower() for i in x["exemplar"]] + ["True, " + i.lower() for i in x["exemplar"]]
-    #     },
-    #     batched=True,
-    #     num_proc=1,
-    #     remove_columns=dataset["train"].column_names
-    # )
+    dataset = dataset.map(
+        lambda x: {
+            text_column: ["All " + i.lower() for i in x["generic_new"]] + ["Not all " + i.lower() for i in x["generic_new"]],
+            label_column: ["False, " + i.lower() for i in x["exemplar"]] + ["True, " + i.lower() for i in x["exemplar"]]
+        },
+        batched=True,
+        num_proc=1,
+        remove_columns=dataset["train"].column_names
+    )
 
-    # # Split the dataset into 80/20 train and test splits randomly with the given random seed
-    # dataset = dataset["train"].train_test_split(test_size=0.2, seed=10)
+    # Split the dataset into 80/20 train and test splits randomly with the given random seed
+    dataset = dataset["train"].train_test_split(test_size=0.2, seed=10)
 
-    # print(dataset)
-    # print(dataset["train"][0])
-    # print(dataset["test"][0])
-
-
+    print(dataset)
+    print(dataset["train"][0])
+    print(dataset["test"][0])
 
 
 
@@ -80,52 +74,54 @@ def main():
 
 
 
-    # print("\n=== Set up the dataset tokenizer ===")
-
-    # tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, token="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")
-    # if tokenizer.pad_token_id is None:
-    #     tokenizer.pad_token_id = tokenizer.eos_token_id
 
 
-    # def preprocess_function(examples):
-    #     batch_size = len(examples[text_column])
+    print("\n=== Set up the dataset tokenizer ===")
 
-    #     # Tokenize the input text and labels
-    #     inputs = [f"{text_column} : {x} Label : " for x in examples[text_column]]
-    #     targets = [str(x) for x in examples[label_column]]
-    #     model_inputs = tokenizer(inputs)
-    #     labels = tokenizer(targets)
+    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, token="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")
+    if tokenizer.pad_token_id is None:
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+
+
+    def preprocess_function(examples):
+        batch_size = len(examples[text_column])
+
+        # Tokenize the input text and labels
+        inputs = [f"{text_column} : {x} Label : " for x in examples[text_column]]
+        targets = [str(x) for x in examples[label_column]]
+        model_inputs = tokenizer(inputs)
+        labels = tokenizer(targets)
         
-    #     # For each example in a batch, pad the labels with the tokernizers pad_token_id
-    #     for i in range(batch_size):
-    #         sample_input_ids = model_inputs["input_ids"][i]
-    #         label_input_ids = labels["input_ids"][i] + [tokenizer.pad_token_id]
-    #         # print(i, sample_input_ids, label_input_ids)
+        # For each example in a batch, pad the labels with the tokernizers pad_token_id
+        for i in range(batch_size):
+            sample_input_ids = model_inputs["input_ids"][i]
+            label_input_ids = labels["input_ids"][i] + [tokenizer.pad_token_id]
+            # print(i, sample_input_ids, label_input_ids)
 
-    #         # Concatenate the input text and labels into the model_inputs.
-    #         model_inputs["input_ids"][i] = sample_input_ids + label_input_ids
-    #         labels["input_ids"][i] = [-100] * len(sample_input_ids) + label_input_ids
+            # Concatenate the input text and labels into the model_inputs.
+            model_inputs["input_ids"][i] = sample_input_ids + label_input_ids
+            labels["input_ids"][i] = [-100] * len(sample_input_ids) + label_input_ids
 
-    #         # Create a separate attention mask for labels and model_inputs.
-    #         model_inputs["attention_mask"][i] = [1] * len(model_inputs["input_ids"][i])
+            # Create a separate attention mask for labels and model_inputs.
+            model_inputs["attention_mask"][i] = [1] * len(model_inputs["input_ids"][i])
         
-    #     # Loop through each example in the batch again to pad the input ids, labels, and attention mask to the max_length and convert them to PyTorch tensors.
-    #     for i in range(batch_size):
-    #         sample_input_ids = model_inputs["input_ids"][i]
-    #         label_input_ids = labels["input_ids"][i]
-    #         model_inputs["input_ids"][i] = [tokenizer.pad_token_id] * (
-    #             max_length - len(sample_input_ids)
-    #         ) + sample_input_ids
-    #         model_inputs["attention_mask"][i] = [0] * (max_length - len(sample_input_ids)) + model_inputs[
-    #             "attention_mask"
-    #         ][i]
-    #         labels["input_ids"][i] = [-100] * (max_length - len(sample_input_ids)) + label_input_ids
-    #         model_inputs["input_ids"][i] = torch.tensor(model_inputs["input_ids"][i][:max_length])
-    #         model_inputs["attention_mask"][i] = torch.tensor(model_inputs["attention_mask"][i][:max_length])
-    #         labels["input_ids"][i] = torch.tensor(labels["input_ids"][i][:max_length])
+        # Loop through each example in the batch again to pad the input ids, labels, and attention mask to the max_length and convert them to PyTorch tensors.
+        for i in range(batch_size):
+            sample_input_ids = model_inputs["input_ids"][i]
+            label_input_ids = labels["input_ids"][i]
+            model_inputs["input_ids"][i] = [tokenizer.pad_token_id] * (
+                max_length - len(sample_input_ids)
+            ) + sample_input_ids
+            model_inputs["attention_mask"][i] = [0] * (max_length - len(sample_input_ids)) + model_inputs[
+                "attention_mask"
+            ][i]
+            labels["input_ids"][i] = [-100] * (max_length - len(sample_input_ids)) + label_input_ids
+            model_inputs["input_ids"][i] = torch.tensor(model_inputs["input_ids"][i][:max_length])
+            model_inputs["attention_mask"][i] = torch.tensor(model_inputs["attention_mask"][i][:max_length])
+            labels["input_ids"][i] = torch.tensor(labels["input_ids"][i][:max_length])
         
-    #     model_inputs["labels"] = labels["input_ids"]
-    #     return model_inputs
+        model_inputs["labels"] = labels["input_ids"]
+        return model_inputs
 
 
 
@@ -133,44 +129,18 @@ def main():
 
 
 
-    # print("\n=== Preprocess dataset ===")
+    print("\n=== Preprocess dataset ===")
 
-    # with accelerator.main_process_first():
-    #     processed_datasets = dataset.map(
-    #         preprocess_function,
-    #         batched=True,
-    #         num_proc=1,
-    #         remove_columns=dataset["train"].column_names,
-    #         load_from_cache_file=False,
-    #         desc="Running tokenizer on dataset",
-    #     )
-    # accelerator.wait_for_everyone()
-
-
-
-
-
-
-
-
-    # print("\n=== Create DataLoader ===")
-
-    # train_dataset = processed_datasets["train"]
-    # train_dataloader = DataLoader(
-    #     train_dataset, shuffle=True, collate_fn=default_data_collator, batch_size=batch_size, pin_memory=True
-    # )
-
-    # eval_dataset = processed_datasets["test"]
-    # eval_dataloader = DataLoader(eval_dataset, collate_fn=default_data_collator, batch_size=batch_size, pin_memory=True)
-
-
-
-
-
-
-    # print("\n=== Initialize optimizer ===")
-
-    # optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+    with accelerator.main_process_first():
+        processed_datasets = dataset.map(
+            preprocess_function,
+            batched=True,
+            num_proc=1,
+            remove_columns=dataset["train"].column_names,
+            load_from_cache_file=False,
+            desc="Running tokenizer on dataset",
+        )
+    accelerator.wait_for_everyone()
 
 
 
@@ -178,13 +148,39 @@ def main():
 
 
 
-    # print("\n=== Initialize Learning Rate scheduler ===")
 
-    # lr_scheduler = get_linear_schedule_with_warmup(
-    #     optimizer=optimizer,
-    #     num_warmup_steps=0,
-    #     num_training_steps=(len(train_dataloader) * num_epochs),
-    # )
+    print("\n=== Create DataLoader ===")
+
+    train_dataset = processed_datasets["train"]
+    train_dataloader = DataLoader(
+        train_dataset, shuffle=True, collate_fn=default_data_collator, batch_size=batch_size, pin_memory=True
+    )
+
+    eval_dataset = processed_datasets["test"]
+    eval_dataloader = DataLoader(eval_dataset, collate_fn=default_data_collator, batch_size=batch_size, pin_memory=True)
+
+
+
+
+
+
+    print("\n=== Initialize optimizer ===")
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+
+
+
+
+
+
+
+    print("\n=== Initialize Learning Rate scheduler ===")
+
+    lr_scheduler = get_linear_schedule_with_warmup(
+        optimizer=optimizer,
+        num_warmup_steps=0,
+        num_training_steps=(len(train_dataloader) * num_epochs),
+    )
 
 
 
@@ -194,8 +190,8 @@ def main():
     print("\n=== Moving model to Accelerator to handles device placement ===")
 
     accelerator.free_memory()
-    model = accelerator.prepare(
-        model
+    model, train_dataloader = accelerator.prepare(
+        model, train_dataloader
     )
     accelerator.print(model)
 
