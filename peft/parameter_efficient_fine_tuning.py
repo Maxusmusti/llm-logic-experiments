1440726c8b2886ad1caf253ddeb91eb3c00513fa
Justin Lee <leezzjus@amazon.com>
2023-11-03 18:24:31 -0700

    Adjust accelrator peft code

--- peft/parameter_efficient_fine_tuning.py
+++ peft/parameter_efficient_fine_tuning.py
@@ -1,8 +1,7 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, LlamaForCausalLM, LlamaTokenizer, default_data_collator, get_linear_schedule_with_warmup
-from peft import get_peft_config, get_peft_model, PromptTuningInit, PromptTuningConfig, TaskType, PeftType
+from transformers import AutoTokenizer, AutoModelForCausalLM, default_data_collator, get_linear_schedule_with_warmup
+from peft import get_peft_model, PromptTuningInit, PromptTuningConfig, TaskType
 import torch
 from datasets import load_dataset
-import os
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from accelerate import Accelerator
@@ -120,16 +119,21 @@ def main():
 
 
 
-    print("\n=== Preprocess dataset ===")
+    print("\n=== Create accelerator and preprocess dataset ===")
 
-    processed_datasets = dataset.map(
-        preprocess_function,
-        batched=True,
-        num_proc=1,
-        remove_columns=dataset["train"].column_names,
-        load_from_cache_file=False,
-        desc="Running tokenizer on dataset",
-    )
+    accelerator = Accelerator()
+    with accelerator.main_process_first():
+        processed_datasets = dataset.map(
+            preprocess_function,
+            batched=True,
+            num_proc=1,
+            remove_columns=dataset["train"].column_names,
+            load_from_cache_file=False,
+            desc="Running tokenizer on dataset",
+        )
+    accelerator.wait_for_everyone()
+
+
 
 
 
@@ -152,24 +156,12 @@ def main():
 
 
 
-    print("\n=== Create Accelerator ===")
-
-    accelerator = Accelerator()
-    with accelerator.main_process_first():
-        processed_datasets = processed_datasets
-    accelerator.wait_for_everyone()
-
-
-
-
-
-
 
     print("\n=== Initialize model ===")
 
     model = AutoModelForCausalLM.from_pretrained(model_name_or_path, token="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")
     model = get_peft_model(model, peft_config)
-    print(model.print_trainable_parameters())
+    model.print_trainable_parameters()
 
 
 
@@ -202,9 +194,7 @@ def main():
 
 
     print("\n=== Moving model to Acceleartor to handles device placement ===")
-    # When using accelerator, replace all instances of .to(device) and loss.backward()
 
-    torch.cuda.empty_cache()
     model, train_dataloader, eval_dataloader, optimizer, lr_scheduler = accelerator.prepare(
         model, train_dataloader, eval_dataloader, optimizer, lr_scheduler
     )
@@ -219,39 +209,39 @@ def main():
 
 
 
-    print("\n=== Training ===")
+    # print("\n=== Training ===")
 
-    for epoch in range(num_epochs):
-        model.train()
-        total_loss = 0
-        for step, batch in enumerate(tqdm(train_dataloader)):
-            print("train loop")
-            outputs = model(**batch)
-            loss = outputs.loss
-            total_loss += loss.detach().float()
-            accelerator.backward(loss)
-            optimizer.step()
-            lr_scheduler.step()
-            optimizer.zero_grad()
+    # for epoch in range(num_epochs):
+    #     model.train()
+    #     total_loss = 0
+    #     for step, batch in enumerate(tqdm(train_dataloader)):
+    #         print("train loop")
+    #         outputs = model(**batch)
+    #         loss = outputs.loss
+    #         total_loss += loss.detach().float()
+    #         accelerator.backward(loss)
+    #         optimizer.step()
+    #         lr_scheduler.step()
+    #         optimizer.zero_grad()
 
-        model.eval()
-        eval_loss = 0
-        eval_preds = []
-        for step, batch in enumerate(tqdm(eval_dataloader)):
-            print("eval loop")
-            with torch.no_grad():
-                outputs = model(**batch)
-            loss = outputs.loss
-            eval_loss += loss.detach().float()
-            eval_preds.extend(
-                tokenizer.batch_decode(torch.argmax(outputs.logits, -1).detach().cpu().numpy(), skip_special_tokens=True)
-            )
+    #     model.eval()
+    #     eval_loss = 0
+    #     eval_preds = []
+    #     for step, batch in enumerate(tqdm(eval_dataloader)):
+    #         print("eval loop")
+    #         with torch.no_grad():
+    #             outputs = model(**batch)
+    #         loss = outputs.loss
+    #         eval_loss += loss.detach().float()
+    #         eval_preds.extend(
+    #             tokenizer.batch_decode(torch.argmax(outputs.logits, -1).detach().cpu().numpy(), skip_special_tokens=True)
+    #         )
 
-        eval_epoch_loss = eval_loss / len(eval_dataloader)
-        eval_ppl = torch.exp(eval_epoch_loss)
-        train_epoch_loss = total_loss / len(train_dataloader)
-        train_ppl = torch.exp(train_epoch_loss)
-        print("epoch=" + epoch, "train_ppl=" + train_ppl, "train_epoch_loss="+ train_epoch_loss, "eval_ppl="+ eval_ppl, "eval_epoch_loss=" + eval_epoch_loss)
+    #     eval_epoch_loss = eval_loss / len(eval_dataloader)
+    #     eval_ppl = torch.exp(eval_epoch_loss)
+    #     train_epoch_loss = total_loss / len(train_dataloader)
+    #     train_ppl = torch.exp(train_epoch_loss)
+    #     print("epoch=" + epoch, "train_ppl=" + train_ppl, "train_epoch_loss="+ train_epoch_loss, "eval_ppl="+ eval_ppl, "eval_epoch_loss=" + eval_epoch_loss)
 
 
 
