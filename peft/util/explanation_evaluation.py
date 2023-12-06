9090e07f3a76a7ad79ca65666c65247f3e20d7aa
Justin Lee <leezzjus@amazon.com>
2023-12-06 10:03:59 -0800

    Evaluation scores for pos and neg separate

--- peft/util/explanation_evaluation.py
+++ peft/util/explanation_evaluation.py
@@ -5,7 +5,7 @@ to evalaute the model explanations on "factual" and "justifies"
     - Results are stored in peft/results/human_evaluation/
 '''
 
-from results_analysis import *
+from accuracy_evaluation import *
 import random
 
 
@@ -15,8 +15,16 @@ def get_evaluation_metric(filename):
     outputs the overall average model explanation quality
     """
     with open(filename) as f:
+
         factual_num, factual_den = 0,0
         justifies_num, justifies_den = 0,0
+
+        pos_factual_num, pos_factual_den = 0,0
+        pos_justifies_num, pos_justifies_den = 0,0
+
+        neg_factual_num, neg_factual_den = 0,0
+        neg_justifies_num, neg_justifies_den = 0,0
+
         for line in f:
             factual = int(line.strip().split(',')[-3])
             justifies = int(line.strip().split(',')[-2])
@@ -25,14 +33,35 @@ def get_evaluation_metric(filename):
             justifies_num += justifies
             factual_den += 1
             justifies_den += 1
+
+            if get_pos_or_neg(line.strip().split(',')[1]) == "neg":
+                neg_factual_num += factual
+                neg_factual_den += 1
+                neg_justifies_num += justifies
+                neg_justifies_den += 1
+            else:
+                pos_factual_num += factual
+                pos_factual_den += 1
+                pos_justifies_num += justifies
+                pos_justifies_den += 1
+                
+
+        overall_score = "N/A" if (factual_den + justifies_den) == 0 else (factual_num + justifies_num)/(factual_den + justifies_den)
+        overall_factual = "N/A" if factual_den == 0 else factual_num/factual_den
+        overall_justifies = "N/A" if justifies_den == 0 else justifies_num/justifies_den
+
+        pos_score = "N/A" if (pos_factual_den + pos_justifies_den) == 0 else (pos_factual_num + pos_justifies_num)/(pos_factual_den + pos_justifies_den)
+        pos_factual = "N/A" if pos_factual_den == 0 else pos_factual_num/pos_factual_den
+        pos_justifies = "N/A" if pos_justifies_den == 0 else pos_justifies_num/pos_justifies_den
+
+        neg_score = "N/A" if (neg_factual_den + neg_justifies_den) == 0 else (neg_factual_num + neg_justifies_num)/(neg_factual_den + neg_justifies_den)
+        neg_factual = "N/A" if neg_factual_den == 0 else neg_factual_num/neg_factual_den
+        neg_justifies = "N/A" if neg_justifies_den == 0 else neg_justifies_num/neg_justifies_den
         
-        try:
-            print("Overall:", (factual_num + justifies_num)/(factual_den + justifies_den))
-            print("Factual:", factual_num/factual_den)
-            print("Justifies:", justifies_num/justifies_den)
-            print()
-        except:
-            pass
+        print(f"{overall_score=} {overall_factual=} {overall_justifies=}")
+        print(f"{pos_score=} {pos_factual=} {pos_justifies=}")
+        print(f"{neg_score=} {neg_factual=} {neg_justifies=}")
+        print()
 
 
 def human_annotation_interface(input_filename, output_filename):
