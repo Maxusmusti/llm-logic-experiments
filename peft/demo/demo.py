f9a988c52f2d8e2ba4fea35d73621dcb14beeacd
Justin Lee <leezzjus@amazon.com>
2023-12-10 19:49:04 -0800

    Minor

--- peft/demo/demo.py
+++ peft/demo/demo.py
@@ -69,96 +69,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, token="hf_qBphNVhG
 if tokenizer.pad_token_id is None:
     tokenizer.pad_token_id = tokenizer.eos_token_id
 
-def preprocess_function(examples):
-    batch_size = len(examples[text_column])
 
-    # Tokenize the input text and labels
-    inputs = [f"{text_column} : {x} Label : " for x in examples[text_column]] # Format the data into an actual natural-language query for the LLM
-    targets = [str(x) for x in examples[label_column]]
-    model_inputs = tokenizer(inputs)
-    labels = tokenizer(targets)
-    
-    # For each example in a batch, pad the labels with the tokernizer's pad_token_id
-    for i in range(batch_size):
-        sample_input_ids = model_inputs["input_ids"][i]
-        label_input_ids = labels["input_ids"][i] + [tokenizer.pad_token_id]
-
-        # Concatenate the input text and labels into the model_inputs.
-        model_inputs["input_ids"][i] = sample_input_ids + label_input_ids
-        labels["input_ids"][i] = [-100] * len(sample_input_ids) + label_input_ids
-
-        # Create a separate attention mask for labels and model_inputs.
-        model_inputs["attention_mask"][i] = [1] * len(model_inputs["input_ids"][i])
-    
-    # Loop through each example in the batch again to pad the input ids, labels, and attention mask to the max_length and convert them to PyTorch tensors.
-    for i in range(batch_size):
-        sample_input_ids = model_inputs["input_ids"][i]
-        label_input_ids = labels["input_ids"][i]
-        model_inputs["input_ids"][i] = [tokenizer.pad_token_id] * (
-            max_length - len(sample_input_ids)
-        ) + sample_input_ids
-        model_inputs["attention_mask"][i] = [0] * (max_length - len(sample_input_ids)) + model_inputs[
-            "attention_mask"
-        ][i]
-        labels["input_ids"][i] = [-100] * (max_length - len(sample_input_ids)) + label_input_ids
-        model_inputs["input_ids"][i] = torch.tensor(model_inputs["input_ids"][i][:max_length])
-        model_inputs["attention_mask"][i] = torch.tensor(model_inputs["attention_mask"][i][:max_length])
-        labels["input_ids"][i] = torch.tensor(labels["input_ids"][i][:max_length])
-    
-    model_inputs["labels"] = labels["input_ids"]
-    return model_inputs
-
-# Apply preprocess function to dataset
-accelerator = Accelerator()
-with accelerator.main_process_first():
-    processed_datasets = dataset.map(
-        preprocess_function,
-        batched=True,
-        num_proc=1,
-        remove_columns=dataset["train"].column_names,
-        load_from_cache_file=False,
-        desc="Running tokenizer on dataset",
-    )
-accelerator.wait_for_everyone()
-
-# Create data loaders
-train_dataset = processed_datasets["train"]
-train_dataloader = DataLoader(
-    train_dataset, shuffle=True, collate_fn=default_data_collator, batch_size=batch_size, pin_memory=True
-)
-
-eval_dataset = processed_datasets["test"]
-eval_dataloader = DataLoader(eval_dataset, collate_fn=default_data_collator, batch_size=batch_size, pin_memory=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-print("\n=== Move model to accelerator to handle device placement ===")
-optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
-lr_scheduler = get_linear_schedule_with_warmup(
-    optimizer=optimizer,
-    num_warmup_steps=0,
-    num_training_steps=(len(train_dataloader) * num_epochs),
-)
-model, train_dataloader, eval_dataloader, optimizer, lr_scheduler = accelerator.prepare(
-    model, train_dataloader, eval_dataloader, optimizer, lr_scheduler
-)
-
-print("\n=== Loading Model ===")
-print("Loading model from", model_load_dir)
-accelerator.load_state(input_dir=model_load_dir)
-
-print("\n=== Evaluate model ===")
 
 # Create data loader for evaluation that intentionally leaves out the answer so the model can fill it in
 def create_evaluation_dataset(examples):
@@ -182,9 +93,51 @@ def create_evaluation_dataset(examples):
     
     return model_inputs
 
-evaluation_dataset = dataset["test"].map(create_evaluation_dataset, batched=True, num_proc=1)
+# Apply preprocess function to dataset
+accelerator = Accelerator()
+with accelerator.main_process_first():
+    evaluation_dataset = dataset["test"].map(
+        create_evaluation_dataset,
+        batched=True,
+        num_proc=1,
+        remove_columns=dataset["train"].column_names,
+        load_from_cache_file=False,
+        desc="Running tokenizer on dataset",
+    )
+accelerator.wait_for_everyone()
+
+# Create data loader
 evaluation_dataset_dataloader = DataLoader(evaluation_dataset, collate_fn=default_data_collator, batch_size=batch_size, pin_memory=True)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("\n=== Move model to accelerator to handle device placement ===")
+optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+lr_scheduler = get_linear_schedule_with_warmup(
+    optimizer=optimizer,
+    num_warmup_steps=0,
+    num_training_steps=(len(evaluation_dataset_dataloader) * num_epochs),
+)
+model, evaluation_dataset_dataloader, optimizer, lr_scheduler = accelerator.prepare(
+    model, evaluation_dataset_dataloader, optimizer, lr_scheduler
+)
+
+print("\n=== Loading Model ===")
+print("Loading model from", model_load_dir)
+accelerator.load_state(input_dir=model_load_dir)
+
+print("\n=== Evaluate model ===")
 eval_preds = []
 for step, batch in enumerate(tqdm(evaluation_dataset_dataloader)):
     batch = {k: v.to("cuda") for k, v in batch.items() if k != "labels"}
