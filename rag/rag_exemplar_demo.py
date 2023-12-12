a0447c276d191e790b899082639e3ae9da8296a0
Mustafa Eyceoz <meyceoz@redhat.com>
2023-12-11 21:49:11 -0500

    Updated RAG demo

--- rag/rag_exemplar_demo.py
+++ rag/rag_exemplar_demo.py
@@ -8,10 +8,12 @@ from model_context import get_anyscale_context
 from templates import custom_template
 
 # Select Model
+print("Loading model context...")
 service_context = get_anyscale_context()
 set_global_service_context(service_context)
 
 # Load embedding store
+print("Loading RAG embeddings...")
 storage_context = StorageContext.from_defaults(persist_dir="vector-db-all")
 index = load_index_from_storage(
     service_context=service_context, storage_context=storage_context
@@ -23,6 +25,6 @@ query_engine = index.as_query_engine(
     similarity_top_k=2,
 )
 
-response = query_engine.query("all birds can fly")
-print("Statement to evaluate: all birds can fly\n")
+print("\nStatement to evaluate: not all Birds depend on vegetation.\n")
+response = query_engine.query("not all Birds depend on vegetation.")
 print(response, "\n")
